69b7574239333d128bac1ac8e6c063f90caea902
atherrien <alexandre.therrien3@gmail.com>
2023-07-21 20:03:49 -0400

    Add test back

--- src/test/java/com/theinitiative/restapp/RestappApplicationTest.java
+++ src/test/java/com/theinitiative/restapp/RestappApplicationTest.java
@@ -15,7 +15,15 @@ public class RestappApplicationTest extends BaseSeleniumTests{
 	@Test
 	public void whenButtonClick_counterIncreases() {
 		driver.get("http://localhost:8080/index.html");
-		
+		WebElement elementButton = driver.findElement(By.id("testButton"));
+		elementButton.click();
+		assertNotNull("The button must not be null", elementButton);
+		new WebDriverWait(driver, java.time.Duration.ofMillis(10000))
+   			.until(ExpectedConditions.textToBe(By.id("messageOutput"), "You have clicked the button 1 times!"));
+
+		elementButton.click();
+		new WebDriverWait(driver, java.time.Duration.ofMillis(10000))
+   			.until(ExpectedConditions.textToBe(By.id("messageOutput"), "You have clicked the button 2 times!"));
 	}
 
 }
